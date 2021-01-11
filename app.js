5f172619f48a2db745c32953e3d15433e64514b9
Passion4film <passion4film@hotmail.co.uk>
2021-01-11 18:00:12 +0000

    refractoring and fixing bug via locking the board between tries

--- app.js
+++ app.js
@@ -1,35 +1,49 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
         // first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
+
+        return;
+    }
         // second click
         hasFlippedCard = false;
         secondCard = this;
 
-        //do cards match?
-        if (firstCard.dataset.framework === 
-            secondCard.dataset.framework) {
-            // it's a match!!
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            // not a match
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500);
-            
-        }
-    }
+        checkForMatch();            
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework ===
+    secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards () {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards () {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
